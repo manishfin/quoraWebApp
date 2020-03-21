c53954514fbd430ccf828e98477d96e520797146
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-21 13:24:54 +0530

    [models] Refactor the code

--- webApp/models.py
+++ webApp/models.py
@@ -2,47 +2,41 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
+    email = models.CharField(max_length=50, unique=True)
     dob = models.DateField()
     GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'))
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['username'], name="username")
-        ]
 
 class Category(models.Model):
-    name = models.CharField(max_lenght=20)
+    name = models.CharField(max_length=20)
     description = models.TextField()
 
 class Question(models.Model):
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    categoryId = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     question = models.TextField()
     upvote = models.IntegerField()
     downvote = models.IntegerField()
-    createdAt = models.DateField()
-    updatedAt = models.DateField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
     type = models.CharField(max_length=30)
     CHOICES = ((0, True),(1, False))
-    isAnonymous = models.IntegerField(max_length=1, default=0, choices=CHOICES)
+    is_anonymous = models.IntegerField(default=0, choices=CHOICES)
 
 class Answer(models.Model):
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    questionId = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.TextField()
-    createdAt = models.DateField()
-    updatedAt = models.DateField()
+    created_at = models.DateField()
+    updated_at = models.DateField()
     CHOICES = ((0, True),(1, False))
-    isAnonymous = models.IntegerField(max_length=1, default=0, choices=CHOICES)
+    is_anonymous = models.IntegerField(default=0, choices=CHOICES)
 
 class Comment(models.Model):
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
-    questionId = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     comment = models.TextField()
     upvote = models.IntegerField()
     downvote = models.IntegerField()
