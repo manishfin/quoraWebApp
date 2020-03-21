6159769e71cf0033117354dd358102740cb43fc0
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-21 16:57:01 +0530

    Change char field to email field

--- webApp/models.py
+++ webApp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
-    email = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
     dob = models.DateField()
     GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'))
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
