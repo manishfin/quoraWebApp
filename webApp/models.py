ca65b9263cba28b28e9e0e139d260c90acb71389
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-23 12:15:40 +0530

    Refactor the code

--- webApp/models.py
+++ webApp/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class BaseClass(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
@@ -9,36 +17,33 @@ class User(AbstractUser):
     GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'))
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True)
 
+
 class Category(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField(null=True)
 
-class Question(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Question(BaseClass):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     question = models.TextField()
     upvote = models.IntegerField(default=0)
     downvote = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    type = models.CharField(max_length=30, null=True)
     CHOICES = ((0, True),(1, False))
     is_anonymous = models.IntegerField(default=0, choices=CHOICES)
 
-class Answer(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Answer(BaseClass):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now_add=True)
     CHOICES = ((0, True),(1, False))
     is_anonymous = models.IntegerField(default=0, choices=CHOICES)
 
-class Comment(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Comment(BaseClass):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     comment = models.TextField()
     upvote = models.IntegerField(default=0)
     downvote = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now_add=True)
