dff52e7bf281ca378a50f6687f9c1959bf636391
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-24 10:39:43 +0530

    Register tables

--- webApp/admin.py
+++ webApp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import User, Question, Answer, Comment, Category
 # Register your models here.
+admin.site.register([User, Question, Answer, Comment, Category])
