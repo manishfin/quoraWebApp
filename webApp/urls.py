85a6ab19c6c3fcc6a46daf44826b38e9ec948665
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-25 10:43:36 +0530

    Add urls

--- webApp/urls.py
+++ webApp/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('add', views.add, name='add'),
+    path('add_question', views.add_question, name='add question'),
+    path('add_answer', views.add_answer, name="add answer"),
+    path('add_comment', views.add_comment, name="add comment"),
     path('home', views.home, name='home'),
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
