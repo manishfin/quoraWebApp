cf95ea87cfb1fbeae4cf5cc05f1736772c5969f4
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-25 10:42:08 +0530

    Add views

--- webApp/views.py
+++ webApp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import auth
-from webApp.models import User, Question
+from webApp.models import User, Question, Answer, Comment
+from django.db import connection
 # Create your views here.
 def home(request):
     if request.user.id is None:
         return redirect('/')
-    questions = Question.objects.all()
-    return render(request, 'home.html', {'questions': questions})
+    questions = Question.objects.filter(answer=None)
+    answers = Answer.objects.all().order_by('-updated_at')
+    comments = Comment.objects.all().order_by('-created_at')
+    return render(request, 'home.html', {'questions': questions, 'answers': answers, 'comments': comments})
 
 
 def login(request):
@@ -43,10 +46,31 @@ def logout(request):
     return redirect('/')
 
 
-def add(request):
+def add_question(request):
     if request.method == 'POST':
         question = request.POST['question']
         user = request.user
         que = Question(question=question, user=user)
         que.save()
+    return redirect('home')
+
+
+def add_answer(request):
+    if request.method == 'POST':
+        answer = request.POST['answer']
+        question = Question.objects.get(id=request.POST['question_id'])
+        user = request.user
+        is_anonymous = False if request.user.is_anonymous else True
+        ans = Answer(question=question, answer=answer, user=user, is_anonymous=is_anonymous)
+        ans.save()
+    return redirect('home')
+
+
+def add_comment(request):
+    if request.method == 'POST':
+        answer = Answer.objects.get(id=request.POST['answer_id'])
+        user = request.user
+        comment = request.POST['comment']
+        comment_obj = Comment(answer=answer, comment=comment, user=user)
+        comment_obj.save()
     return redirect('home')
