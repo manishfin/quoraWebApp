23c792509edda7f2ed698eedacc7416a61b1a761
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-26 10:50:48 +0530

    Add views and refactor the code

--- webApp/views.py
+++ webApp/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import auth
-from webApp.models import User, Question, Answer, Comment
-from django.db import connection
+from django.contrib import messages
+from webApp.models import User, Question, Answer, Comment, Vote
+from .forms import UserSignUpForm
+
+
 # Create your views here.
 def home(request):
-    if request.user.id is None:
+    if not request.user.is_authenticated:
         return redirect('/')
-    questions = Question.objects.filter(answer=None)
-    answers = Answer.objects.all().order_by('-updated_at')
     comments = Comment.objects.all().order_by('-created_at')
-    return render(request, 'home.html', {'questions': questions, 'answers': answers, 'comments': comments})
+    items = Question.objects.prefetch_related('answer_set')
+    return render(request, 'home.html', {'items': items, 'comments': comments})
+
+
+def question(request, que_slug):
+    try:
+        question = get_object_or_404(Question, slug=que_slug)
+    except Question.DoesNotExist:
+        question = None
+    try:
+        answer = Answer.objects.get(question=question)
+    except Answer.DoesNotExist:
+        answer = None
+    comments = Comment.objects.filter(answer=answer)
+    upvote_count = Vote.objects.filter(answer=answer, upvote=True).count()
+    downvote_count = Vote.objects.filter(answer=answer, downvote=True).count()
+    context = {
+        'question': question,
+        'answer': answer,
+        'comments': comments,
+        'comment_count': comments.count(),
+        'upvote_count': upvote_count,
+        'downvote_count': downvote_count,
+    }
+    return render(request, 'question.html', context)
 
 
 def login(request):
@@ -20,25 +46,21 @@ def login(request):
         user = auth.authenticate(username=username, password=password)
         if user:
             auth.login(request, user)
+            # messages.success(request, 'Login Success!')
             return redirect('home')
 
-        return render(request, 'login.html')
     return render(request, 'login.html')
 
 
 def signup(request):
-    if request.method == 'POST':
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        email = request.POST['email']
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-        user.save()
+    form = UserSignUpForm(request.POST or None)
+    if form.is_valid():
+        form.save()
         return redirect('login')
-    
-    return render(request, 'signup.html')
+    context = {
+        'form': form,
+    }
+    return render(request, 'signup.html', context)
 
 
 def logout(request):
@@ -47,30 +69,96 @@ def logout(request):
 
 
 def add_question(request):
-    if request.method == 'POST':
-        question = request.POST['question']
-        user = request.user
-        que = Question(question=question, user=user)
-        que.save()
+    user = request.user
+    if user.is_authenticated:
+        if request.method == 'POST':
+            question = request.POST['question']
+            que = Question(question=question, user=user)
+            que.save()
+            messages.success(request, 'Question added successfully!')
+    else:
+        messages.error(request, 'Login required!')
     return redirect('home')
 
 
-def add_answer(request):
-    if request.method == 'POST':
-        answer = request.POST['answer']
-        question = Question.objects.get(id=request.POST['question_id'])
-        user = request.user
-        is_anonymous = False if request.user.is_anonymous else True
-        ans = Answer(question=question, answer=answer, user=user, is_anonymous=is_anonymous)
-        ans.save()
+def add_answer(request, que_slug):
+    user = request.user
+    if user.is_authenticated:
+        if request.method == 'POST':
+            answer = request.POST['answer']
+            question = Question.objects.get(slug=que_slug)
+            is_anonymous = False if request.user.is_anonymous else True
+            ans = Answer(question=question, answer=answer, user=user, is_anonymous=is_anonymous)
+            ans.save()
+            messages.success(request, 'Answer added successfully!')
+    else:
+        messages.error(request, 'Login required!')
     return redirect('home')
 
 
 def add_comment(request):
-    if request.method == 'POST':
-        answer = Answer.objects.get(id=request.POST['answer_id'])
-        user = request.user
-        comment = request.POST['comment']
-        comment_obj = Comment(answer=answer, comment=comment, user=user)
-        comment_obj.save()
-    return redirect('home')
+    user = request.user
+    if user.is_authenticated:
+        if request.method == 'POST':
+            answer = Answer.objects.get(id=request.POST['answer_id'])
+            comment = request.POST['comment']
+            comment_obj = Comment(answer=answer, comment=comment, user=user)
+            comment_obj.save()
+            messages.success(request, 'Comment added successfully!')
+    else:
+        messages.error(request, 'Login required!')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER')) or redirect('home')
+
+
+def add_upvote(request, answer_id):
+    user = request.user
+    if user.is_authenticated:
+        try:
+            answer = Answer.objects.get(id=answer_id)
+        except Answer.DoesNotExist:
+            answer = None
+        if answer:
+            vote = Vote.objects.filter(user=user, answer=answer).first()
+            if vote:
+                vote.upvote = not vote.upvote
+                vote.downvote = False
+            else:
+                vote = Vote(user=request.user, upvote=True, answer=answer)
+            if vote.upvote:
+                messages.info(request, 'You upvoted for this answer!')
+            else:
+                messages.info(request, 'You removed upvote for this answer!')
+            vote.save()
+    else:
+        messages.error(request, 'Login required!')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER')) or redirect('home')
+
+
+def add_downvote(request, answer_id):
+    user = request.user
+    if user.is_authenticated:
+        try:
+            answer = Answer.objects.get(id=answer_id)
+        except Answer.DoesNotExist:
+            answer = None
+        if answer:
+            vote = Vote.objects.filter(user=user, answer=answer).first()
+            if vote:
+                vote.downvote = not vote.downvote
+                vote.upvote = False
+            else:
+                vote = Vote(user=request.user, downvote=True, answer=answer)
+            if vote.downvote:
+                messages.info(request, 'You downvoted for this answer!')
+            else:
+                messages.info(request, 'You removed downvote for this answer!')
+            vote.save()
+    else:
+        messages.error(request, 'Login required!')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER')) or redirect('home')
+
+
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('/')
+    return render(request, 'profile.html')
