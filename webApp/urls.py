a431eaefd0680f4711fffb9088bf9501a76f530e
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-03-26 10:50:11 +0530

    Add urls

--- webApp/urls.py
+++ webApp/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
     path('add_question', views.add_question, name='add question'),
-    path('add_answer', views.add_answer, name="add answer"),
+    path('add_answer/<str:que_slug>', views.add_answer, name="add answer"),
+    path('question/<str:que_slug>', views.question, name="question"),
     path('add_comment', views.add_comment, name="add comment"),
+    path('<int:answer_id>/add_upvote', views.add_upvote, name="add upvote"),
+    path('<int:answer_id>/add_downvote', views.add_downvote, name="add downvote"),
     path('home', views.home, name='home'),
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
     path('logout', views.logout, name='logout'),
+    path('profile', views.profile, name='profile'),
 ]
