a2f319abfdacc61e2c0b5fcca0e56f44cb78f876
Manish Sharma <manishsharma@Manishs-MacBook-Air.local>
2020-04-01 18:53:40 +0530

    Add forms

--- web_app/forms.py
+++ web_app/forms.py
@@ -12,4 +12,33 @@ class UserSignUpForm(UserCreationForm):
             'email',
             'username',
         ]
-        widgets = {}
+        widgets = {
+        }
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['question']
+        widgets = {
+            'question': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Add Question'},
+            )
+        }
+        labels = {
+            'question': '',
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['answer']
+        widgets = {
+            'answer': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Add Answer'},
+            )
+        }
+        labels = {
+            'answer': '',
+        }
